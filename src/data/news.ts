0eccc50ab1453bffc1a909e1c4e737d7476d656e
abreuandre <andreluismachadoabreu@gmail.com>
2026-07-23 14:54:49 -0300

    Adicionado as informações de adoração, dizimista e terço das mulheres nas notícias do site.

--- src/data/news.ts
+++ src/data/news.ts
@@ -7,30 +7,6 @@ export const newsData: Pick<
 >[] = [
   {
     id: 1,
-    slug: "festa-padroeira-2025",
-    date: "",
-    imageUrl:
-      "https://images.unsplash.com/photo-1548625149-720754874919?w=800&q=80",
-    tags: ["Festa", "Padroeira", "Quermesse", "Procissão"],
-  },
-  {
-    id: 2,
-    slug: "catequese-2025-inscricoes",
-    date: "",
-    imageUrl:
-      "https://images.unsplash.com/photo-1529070538774-1843cb3265df?w=800&q=80",
-    tags: ["Catequese", "Primeira Eucaristia", "Crisma", "Inscrições"],
-  },
-  {
-    id: 3,
-    slug: "retiro-quaresma-2025",
-    date: "",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519389950473-47ba0277781c?w=800&q=80",
-    tags: ["Quaresma", "Retiro", "Espiritualidade", "Jovens"],
-  },
-  {
-    id: 4,
     slug: "circulo-biblico",
     date: "",
     imageUrl:
@@ -38,11 +14,67 @@ export const newsData: Pick<
     tags: ["BibliaSagrada", "PalavraDeDeus", "Espiritualidade", "Católicos"],
   },
   {
-    id: 5,
+    id: 2,
     slug: "confissao",
     date: "",
     imageUrl:
       "/confissao.jpeg",
     tags: ["Confissão", "Reconciliação", "Espiritualidade", "VidaCristã"],
   },
+  {
+    id: 3,
+    slug: "terco-homens",
+    date: "",
+    imageUrl:
+      "/terco-dos-homens.jpeg",
+    tags: ["Oração", "Terço", "FéCatólica", "VidaCristã"],
+  },
+  {
+    id: 4,
+    slug: "adoracao",
+    date: "",
+    imageUrl:
+      "/adoracao.jpeg",
+    tags: ["Oração", "Adoração", "FéCatólica", "Conversão"],
+  },
+  {
+    id: 5,
+    slug: "dizimista",
+    date: "",
+    imageUrl:
+      "/dizimista.jpeg",
+    tags: ["Dízimo", "Comunidade", "FéCatólica", "VidaCristã"],
+  },
+  {
+    id: 6,
+    slug: "terco-mulheres",
+    date: "",
+    imageUrl:
+      "/terco-das-mulheres.jpeg",
+    tags: ["Oração", "Terço", "FéCatólica", "VidaCristã"],
+  },
+  {
+    id: 7,
+    slug: "festa-padroeira-2025",
+    date: "",
+    imageUrl:
+      "https://images.unsplash.com/photo-1548625149-720754874919?w=800&q=80",
+    tags: ["Festa", "Padroeira", "Quermesse", "Procissão"],
+  },
+  {
+    id: 8,
+    slug: "catequese-2025-inscricoes",
+    date: "",
+    imageUrl:
+      "https://images.unsplash.com/photo-1529070538774-1843cb3265df?w=800&q=80",
+    tags: ["Catequese", "Primeira Eucaristia", "Crisma", "Inscrições"],
+  },
+  {
+    id: 9,
+    slug: "retiro-quaresma-2025",
+    date: "",
+    imageUrl:
+      "https://images.unsplash.com/photo-1519389950473-47ba0277781c?w=800&q=80",
+    tags: ["Quaresma", "Retiro", "Espiritualidade", "Jovens"],
+  }
 ];
